01cd5e73bdaa0fa8440ab8a8733f85268831a75e
Your Name <you@example.com>
2020-05-28 23:05:23 +0800

    2020-05-28 push

--- src/main/java/DRMCBot/Bot.java
+++ src/main/java/DRMCBot/Bot.java
@@ -1,4 +1,21 @@
 package DRMCBot;
 
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+
+import javax.security.auth.login.LoginException;
+
 public class Bot {
+
+    private Bot() throws LoginException {
+        new JDABuilder()
+                .setToken(Config.get("token"))
+                .addEventListeners(new Listener())
+                .setActivity(Activity.playing("Welcome to New DL/RS/MC Chatroom"))
+                .build();
+
+    }
+    public static void main(String[] args) throws LoginException {
+        new Bot();
+    }
 }
--- src/main/java/DRMCBot/Listener.java
+++ src/main/java/DRMCBot/Listener.java
@@ -1,4 +1,32 @@
 package DRMCBot;
 
-public class Listener {
+import me.duncte123.botcommons.BotCommons;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nonnull;
+
+public class Listener extends ListenerAdapter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Listener.class);
+
+    @Override
+    public void onReady(@Nonnull ReadyEvent event) {
+        LOGGER.info(event.getJDA().getSelfUser().getAsTag()+" is ready");
+    }
+
+    @Override
+    public void onGuildMessageReceived(@Nonnull GuildMessageReceivedEvent event) {
+        String prefix=Config.get("prefix");
+        String raw=event.getMessage().getContentRaw();
+
+        if (raw.equalsIgnoreCase(prefix+"shutdown")){
+            LOGGER.info("Shutting Down");
+            event.getJDA().shutdown();
+            BotCommons.shutdown(event.getJDA());
+        }
+    }
 }
